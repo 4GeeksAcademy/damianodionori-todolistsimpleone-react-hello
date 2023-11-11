4aefb25f7cb6c16dfe5feea074188d24f2c29bec
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-11-11 16:58:25 +0000

    todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const addTask = () => {
+    if (taskInput.trim() !== '') {
+      setTasks([...tasks, taskInput]);
+      setTaskInput('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div id="todo-list">
+      <h1>TODO List</h1>
+      <input
+        type="text"
+        placeholder="Add a new task"
+        value={taskInput}
+        onChange={(e) => setTaskInput(e.target.value)}
+        onKeyUp={(e) => {
+          if (e.key === 'Enter') addTask();
+        }}
+      />
+      <ul id="task-list">
+        {tasks.length === 0 ? (
+          <p id="no-tasks">No tasks, add a task</p>
+        ) : (
+          tasks.map((task, index) => (
+            <li key={index}>
+              <span>{task}</span>
+              <span className="delete-task" onClick={() => deleteTask(index)}>
+                ❌
+              </span>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
 };
 
-export default Home;
+export default TodoList;
